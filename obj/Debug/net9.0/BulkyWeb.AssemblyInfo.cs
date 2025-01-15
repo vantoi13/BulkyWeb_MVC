e111b8f63f3e17c3cec3d18c9529d023ba4fecc3
Toidz <vantoi0103@gmail.com>
2025-01-15 22:11:34 +0700

    a

--- obj/Debug/net9.0/BulkyWeb.AssemblyInfo.cs
+++ obj/Debug/net9.0/BulkyWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02a4503ae42e16f948cac09ea1c94145b0a294e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c2ceea1cb08cb7cf21d9280e24113a381a31ad5")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
